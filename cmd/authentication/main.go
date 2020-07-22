8f4a144856e14a46ae11b9b1265884766001b1a9
Владимир Павлунин <v.pavlunin@litebox.ru>
2020-07-22 21:43:23 +0400

    init

---
+++ cmd/authentication/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// load application configurations
+	if err := config.LoadConfig("./config"); err != nil {
+		panic(fmt.Errorf("invalid application configuration: %s", err))
+	}
+
+	fmt.Println(config.Config.ConfigVar)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module example.com/m/v2
+
+go 1.13
+
+require (
+	github.com/MartinHeinz/go-project-blueprint v0.0.0-20200101162543-93e6e8cc7011 // indirect
+	github.com/spf13/viper v1.7.0
+)
